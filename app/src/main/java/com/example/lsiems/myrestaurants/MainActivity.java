41654d2dda1a0a19ebe655e43a1b06f728014047
Christopher Siems <csiems@gmail.com>
2016-03-12 18:26:44 -0800

    Add zip code entry field

--- app/src/main/java/com/example/lsiems/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/lsiems/myrestaurants/MainActivity.java
@@ -1,27 +1,36 @@
 package com.example.lsiems.myrestaurants;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindRestaurantsButton;
-
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); //causes android to run default actions for activity
         setContentView(R.layout.activity_main); //tells android which layout to use (R is resources)
 
+
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                String location = mLocationEditText.getText().toString();
+                Log.d(TAG, location);
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                startActivity(intent);
             }
+
         });
     }
 }
