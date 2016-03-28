25f8eb36b2f3e751be07260a24e080063ffba6b0
csiems@gmail.com and illia kuchko <ikuchko@gmail.com>
2016-03-28 14:18:08 -0700

    add facebook authentication

--- app/src/main/java/com/example/lsiems/myrestaurants/ui/LoginActivity.java
+++ app/src/main/java/com/example/lsiems/myrestaurants/ui/LoginActivity.java
@@ -12,39 +12,105 @@ import android.widget.EditText;
 
 import com.example.lsiems.myrestaurants.MyRestaurantsApplication;
 import com.example.lsiems.myrestaurants.R;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-    @Bind(R.id.passwordLoginButton) Button mPasswordLoginButton;
+
+    //General
     @Bind(R.id.emailEditText) EditText mEmailEditText;
     @Bind(R.id.passwordEditText) EditText mPasswordEditText;
     @Bind(R.id.registerButton) Button mRegisterButton;
-
     private Firebase mFirebaseRef;
     private Firebase.AuthResultHandler mAuthResultHandler;
     private ProgressDialog mAuthProgressDialog;
+    private Firebase.AuthStateListener mAuthStateListener;
+
+    //Facebook Login Variables
+    @Bind(R.id.facebook_login_button) LoginButton mFacebookButton;
+    private CallbackManager mFacebookCallbackManager;
+    private AccessTokenTracker mFacebookAccessTokenTracker;
+
+    //Password Login Variables
+    @Bind(R.id.passwordLoginButton) Button mPasswordLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
+        //General
         mFirebaseRef = MyRestaurantsApplication.getAppInstance().getFirebaseRef();
-
-        mPasswordLoginButton.setOnClickListener(this);
         mRegisterButton.setOnClickListener(this);
-
         initializeProgressDialog();
         initializeAuthResultHandler();
 
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setTitle("Loading");
+        mAuthProgressDialog.setMessage("Authenticating with Firebase...");
+        mAuthProgressDialog.setCancelable(false);
+        mAuthProgressDialog.show();
+
+        mAuthStateListener = new Firebase.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(AuthData authData) {
+                mAuthProgressDialog.hide();
+
+
+            }
+        };
+        mFirebaseRef.addAuthStateListener(mAuthStateListener);
+
+
+        //Facebook
+        mFacebookCallbackManager = CallbackManager.Factory.create();
+        mFacebookButton.setOnClickListener(this);
+        mFacebookAccessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                LoginActivity.this.onFacebookAccessTokenChange(currentAccessToken);
+            }
+        };
+
+        //Password
+        mPasswordLoginButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // if user logged in with Facebook, stop tracking their token
+        if (mFacebookAccessTokenTracker != null) {
+            mFacebookAccessTokenTracker.stopTracking();
+        }
+
+        // if changing configurations, stop tracking firebase session.
+        mFirebaseRef.removeAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+            mFacebookCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
@@ -74,6 +140,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         mFirebaseRef.authWithPassword(email, password, mAuthResultHandler);
     }
 
+
     private void registerNewUser() {
         mAuthProgressDialog.show();
         final String email = mEmailEditText.getText().toString();
@@ -91,6 +158,43 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             }
         } );
     }
+
+    private class AuthResultHandler implements Firebase.AuthResultHandler {
+
+        private final String provider;
+
+        public AuthResultHandler(String provider) {
+            this.provider = provider;
+        }
+
+        @Override
+        public void onAuthenticated(AuthData authData) {
+            mAuthProgressDialog.hide();
+            goToMainActivity();
+
+        }
+
+        @Override
+        public void onAuthenticationError(FirebaseError firebaseError) {
+            mAuthProgressDialog.hide();
+            showErrorDialog(firebaseError.toString());
+        }
+    }
+
+    private void onFacebookAccessTokenChange(AccessToken token) {
+        if (token != null) {
+            mAuthProgressDialog.show();
+            mFirebaseRef.authWithOAuthToken("facebook", token.getToken(), new AuthResultHandler("facebook"));
+        } else {
+            // Logged out of Facebook and currently authenticated with Firebase using Facebook, so do a logout
+            if (mFirebaseRef.getAuth() != null && mFirebaseRef.getAuth().getProvider().equals("facebook")) {
+                mFirebaseRef.unauth();
+            }
+        }
+    }
+
+
+
 
     private void initializeProgressDialog() {
         mAuthProgressDialog = new ProgressDialog(this);
@@ -122,4 +226,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             | Intent.FLAG_ACTIVITY_NO_HISTORY);
         startActivity(intent);
     }
+
+
 }
